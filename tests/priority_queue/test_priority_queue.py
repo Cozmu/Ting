de16ad2d729232f6ade8e7c673f38fd9710a6e93
Cozmu <www.jorgereis@gmail.com>
2023-06-14 20:16:48 -0300

    feat: implementando exists_word

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,17 +1,19 @@
 from ting_file_management.priority_queue import PriorityQueue
 import pytest
 
+
 def test_basic_priority_queueing():
-    instance_priory_queue = PriorityQueue() 
-    instance_priory_queue.enqueue({ "qtd_linhas": 6 })
-    instance_priory_queue.enqueue({ "qtd_linhas": 3 })
+
+    instance_priory_queue = PriorityQueue()
+    instance_priory_queue.enqueue({"qtd_linhas": 6})
+    instance_priory_queue.enqueue({"qtd_linhas": 3})
 
     assert instance_priory_queue.__len__() == 2
-    assert instance_priory_queue.dequeue() == { "qtd_linhas": 3 }
+    assert instance_priory_queue.dequeue() == {"qtd_linhas": 3}
 
     with pytest.raises(IndexError):
         instance_priory_queue.search(-1)
 
-    instance_priory_queue.enqueue({ "qtd_linhas": 1 })
+    instance_priory_queue.enqueue({"qtd_linhas": 1})
 
-    assert instance_priory_queue.search(0) == { "qtd_linhas": 1 }
+    assert instance_priory_queue.search(0) == {"qtd_linhas": 1}
--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,29 @@
+def find_occurrences(lines_list_file, word):
+    contador = 1
+    result = []
+    for line in lines_list_file:
+        if word.lower() in line.lower():
+            result.append({"linha": contador})
+
+        contador += 1
+
+    return result
+
+
 def exists_word(word, instance):
-    with open(path_file, 'r') as file:
-        content = file.read()
+    result = []
+    for dict_file in instance.get_list():
+        occurrences = find_occurrences(dict_file["linhas_do_arquivo"], word)
+        if len(occurrences) != 0:
+            dict_result = {
+                "palavra": word,
+                "arquivo": dict_file["nome_do_arquivo"],
+                "ocorrencias": occurrences
+            }
+            result.append(dict_result)
+
+    return result
+
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
