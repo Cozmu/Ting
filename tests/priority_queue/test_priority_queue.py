10e8d540c313014e62b0b898836802ec6807cffe
Cozmu <www.jorgereis@gmail.com>
2023-06-14 17:55:26 -0300

    test: implementando testes da priority_queue

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,17 @@
-# from ting_file_management.priority_queue import PriorityQueue
-
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    instance_priory_queue = PriorityQueue() 
+    instance_priory_queue.enqueue({ "qtd_linhas": 6 })
+    instance_priory_queue.enqueue({ "qtd_linhas": 3 })
+
+    assert instance_priory_queue.__len__() == 2
+    assert instance_priory_queue.dequeue() == { "qtd_linhas": 3 }
+
+    with pytest.raises(IndexError):
+        instance_priory_queue.search(-1)
+
+    instance_priory_queue.enqueue({ "qtd_linhas": 1 })
+
+    assert instance_priory_queue.search(0) == { "qtd_linhas": 1 }
--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,6 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
-
+    with open(path_file, 'r') as file:
+        content = file.read()
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
